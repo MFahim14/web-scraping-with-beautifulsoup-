89fe1a101958430f9d60087480030b649f4a17de
MFahim14 <mailmefahim14@gmail.com>
2024-09-02 22:27:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,19 +15,6 @@ all_movies = soup.find_all(name="h3", class_="title")
 movie_titles = [movie.getText() for movie in all_movies]
 movies = movie_titles[::-1]
 
-with open("movies.txt", mode="w") as file:
+with open("movies.txt", mode="w",encoding="utf-8") as file:
     for movie in movies:
         file.write(f"{movie}\n")
-
-
-'''
-FAQ: Empire's website has changed!
-
-When this lesson was created, I used this URL for the project: 
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
-
-However, Empire has since changed their website. You can see this when you inspect the movie title elements. 
-You'll see that the h3 with the class "title" is no longer there. 
-To use exactly the same code as per the solution, we can use a cached version of the website from the Internet Archive's Wayback Machine.
-
-'''
